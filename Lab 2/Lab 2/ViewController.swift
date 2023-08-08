35b8991b296f1c99dd2411384febb74c6a12bb9b
Shaima Alharbi <109450745+ShaimaAlharbi0@users.noreply.github.com>
2023-08-08 21:30:31 +0300

    Add files via upload

---
+++ Lab 2/Lab 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lab 2
+//
+//  Created by Shaima Alharbi on 21/01/1445 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var myLabel1: UILabel!
+    @IBOutlet weak var myLabel2: UILabel!
+    @IBOutlet weak var myLabel3: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    func myLabel1.text = textField.text
+    func myLabel2.text = textField.text
+    func myLabel3.text = textField.text
+}
+    
+}
+
